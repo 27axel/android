f6fa75fe3b289fad2449d1ce67f700c9ada2b706
axel <lehander2016@ya.ru>
2023-05-14 18:54:25 +0300

    init commit

---
+++ app/src/main/java/ru/viknist/aston_dz_3/SecondTaskActivity.kt
@@ -0,0 +1,11 @@
+package ru.viknist.aston_dz_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_task)
+    }
+}
